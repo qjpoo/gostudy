393faa3435e8e028b171a24062cb99450295bd9b
qujian <qujian@51-dg.com>
2020-08-03 09:59:09 +0800

    add

--- s105/demo2/main.go
+++ s105/demo2/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
 func main() {
 
-	s, t := "hello go", "hello Go"
+	s, t := "hello go", "helloo Go"
 	isEqual := strings.EqualFold(s, t)
 	fmt.Println(isEqual)
 
@@ -30,14 +31,110 @@ func main() {
 	conRun := strings.ContainsRune(s, r)
 	fmt.Println(conRun)
 
+	// 子串sep在字符串s中第一次出现的位置，不存在就返回-1
 	sep := "0"
 	sepIndex := strings.Index(s, sep)
 	fmt.Println(sepIndex)
 
+	// 子串sepLast在字符串中最后一次出现的位置， 不存在则返回-1
+	sepLast := "o"
+	sepLastPo := strings.LastIndex(s, sepLast)
+	fmt.Println(sepLastPo)
+
+	// title格式，首字母都是大写
+	s1 := "hEllo, quinn"
+	t1 := strings.Title(s1)
+	fmt.Println(t1) // Hello, Quinn
+
+	// 都变成大写
+	t2 := strings.ToTitle(s1)
+	fmt.Println(t2) // HELLO, QUINN
+
+	// 大写变成小写
+	t4 := strings.ToLower(s1)
+	fmt.Println(t4)
+
+	// 重复生成字符串
+	t5 := strings.Repeat("xxoo ", 5)
+	fmt.Println(t5)
+
+	// // 返回将s中前n个不重叠old子串都替换为new的新字符串，如果n<0会替换所有old子串
+	s2 := "go, go, go, to, study"
+	s_old, s_new := ",", "#"
+	t6 := strings.Replace(s2, s_old, s_new, -1)
+	fmt.Println(t6)
+
+	// 去掉字符串前后指定的字符串
+	sn, cut := " hello, world   ", " "
+	t7 := strings.Trim(sn, cut)
+	fmt.Println(t7)
+
+	t8 := strings.TrimLeft("#/bin/bash", "#")
+	fmt.Println(t8)
+
+	t9 := strings.TrimPrefix("/root/scriptes/hello.sh", "/root")
+	fmt.Println(t9)
+
+	t10 := strings.TrimSpace("  hello, world  ")
+	fmt.Println(t10 + "S")
+
+	// 返回将字符串按照空白（unicode.IsSpace确定，可以是一到多个连续的空白字符）分割的多个字符串
+	strSplit := strings.Fields("hello world!     go language ...")
+	for k, v := range strSplit {
+		fmt.Println(k, "====>", v)
+	}
+
+	fmt.Println("==========================")
+	ssplit := strings.Split("//root/scripts/20200731/mysql.sh", "/")
+	for k, v := range ssplit {
+		fmt.Println(k, "====>", v)
+	}
+
+	//  // 将一系列字符串连接为一个字符串，之间用sep来分隔
+	j := strings.Join([]string{"a", "b", "c", "d"}, "@")
+	fmt.Println(j)
+
+	// 将s的每一个unicode码值r都替换为mapping(r)，返回这些新码值组成的字符串拷贝。如果mapping返回一个负值，将会丢弃该码值而不会被替换
+	map_func := func(r rune) rune {
+		switch {
+		case r > 'A' && r < 'Z':
+			return r + 32
+		case r > 'a' && r < 'z':
+			return r - 32
+		}
+		return r
+	}
+	s = "Hello World!"
+	s_map := strings.Map(map_func, s)
+	fmt.Println(s_map) // hELLO wORLD!
+
+	// Reader 类型从一个字符串读取数据，实现了io.Reader, io.Seeker等接口。
+	newR := strings.NewReader("123456789")
+	fmt.Println(reflect.TypeOf(newR), newR)
+	fmt.Println(newR.Len())  // 获取未读取的长度
+	fmt.Println(newR.Size()) //  获取字符串的长度
+
+	for newR.Len() > 5 {
+		b, err := newR.ReadByte() // 读取1 byte
+		fmt.Println(string(b), err, newR.Len(), newR.Size())
+		/*
+		1 <nil> 8 9
+		2 <nil> 7 9
+		3 <nil> 6 9
+		4 <nil> 5 9
+		*/
+	}
 
 
+	b_s :=make([]byte, 5)
+	n, err := newR.Read(b_s)
+	fmt.Println(string(b_s), n, err)  // 56789 5 <nil>
+	fmt.Println(newR.Size()) // 9
+	fmt.Println(newR.Len())  // 0
 
 
-
-
+	si := "go hello"
+	ra := strings.NewReplacer("g", "G", "h", "j")
+	fmt.Println(reflect.TypeOf(ra))
+	fmt.Println(ra.Replace(si))
 }
--- s105/demo3/main.go
+++ s105/demo3/main.go
@@ -1,1 +1,107 @@
-package demo3
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"reflect"
+)
+
+func main() {
+	// path package
+
+	// 判断是否是一个绝对路径
+	pt := "/root/home/x1/test/hello.sh"
+	is1 := path.IsAbs(pt)
+	fmt.Println(is1)
+
+	// 将路径分割为路径和文件名
+	dir, file := path.Split(pt)
+	fmt.Println(dir, file) // /root/home/x1/ test
+
+	// 将多个字符串合并为一个路径
+	jo := path.Join("/usr", "local", "src")
+	fmt.Println(jo)
+
+	// 返回路径中扩展
+	fmt.Println(path.Ext(pt)) // .sh
+
+	// 返回路径的最后一个元素
+	fmt.Println(path.Base(pt)) // hello.sh
+
+	// 返回目录
+	fmt.Println(path.Dir(pt)) // /root/home/x1/test
+
+	// 返回目录中的最短路径
+	dira := "/usr/../hello/../local/src/ssh"
+	fmt.Println(path.Clean(dira)) // /local/src/ssh
+
+	// 正则是否匹配路径
+	isMatch, err := path.Match(".*.sh", "/root/hello.sh")
+	fmt.Println(isMatch, err)
+
+	/*var arr = [...]int{5:2}
+	fmt.Println(arr)
+	var pf *[6]int = &arr
+	fmt.Println((*pf)[5])*/
+
+	// ------------------------------------------------------
+	// path/filepath 包
+	b1 := filepath.Base("/root/home/file.go")
+	fmt.Println(b1)
+	sep := filepath.Separator
+	fmt.Println(reflect.TypeOf(sep), string(sep)) // int32 run
+	fmt.Println(string(101))
+	// 预定义变量  预定义变量，表示路径分隔符 /  预定义变量，表示环境变量分隔符 :
+	// \ ; \ ;
+	fmt.Println(string(filepath.Separator), string(filepath.ListSeparator), string(os.PathSeparator), string(os.PathListSeparator))
+
+	// 返回绝对路径
+	dir1 := "C:\\Users\\86137\\godfd\\df"
+	real_dir, err := filepath.Abs(dir1)
+	fmt.Println(real_dir, err)
+
+	// 返回targpath 相对 basepath路径
+	basePath, targetPath := "/usr/local", "/usr/local/go/bin"
+	relDir, err := filepath.Rel(basePath, targetPath)
+	fmt.Println(relDir, err) // go\bin
+
+	fmt.Println("------------------------")
+	// 返回路径最前面的卷名
+	root := "/usr/local/go"
+	vol := filepath.VolumeName(root)
+	fmt.Println(vol) // ''
+
+	// 路径分隔符替换为 `/`
+	slash_dir := filepath.ToSlash(root)
+	fmt.Println(slash_dir) // /usr/local/go
+
+	//  `/` 替换为路径分隔符
+	from_slash := filepath.FromSlash(slash_dir)
+	fmt.Println(from_slash) // /usr/local/go
+
+
+
+	targetPath1 := os.Getenv("path")
+	pathSlice1 := filepath.SplitList(targetPath1)
+	for _, v := range pathSlice1 {
+		fmt.Println(v)
+	}
+
+	// 遍历当前目录的文件树
+	rootDir, err := os.Getwd()
+	err = filepath.Walk(rootDir, pathName)
+	fmt.Println(err)
+
+}
+func pathName(path string, info os.FileInfo, err error) error  {
+	if err != nil {
+		return err
+	}else {
+		fmt.Println(path)
+		fmt.Println(info.Size())
+	}
+	return nil
+
+}
